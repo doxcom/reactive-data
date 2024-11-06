8457b1ac99bb225cdffbc16bd20709ae1f380923
doxcom <aldoperez1319@gmail.com>
2024-11-06 12:51:03 -0600

    initial files to java reactive mongodb

---
+++ src/main/java/com/example/reactive_data/ReactiveDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactive_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactive_data/ReactiveDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
