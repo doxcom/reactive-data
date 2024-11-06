85032215ac526847463d70a071760e1f915680ae
doxcom <aldoperez1319@gmail.com>
2024-11-06 12:55:33 -0600

    repository and document for Persistance

--- src/main/java/com/example/reactive_data/ReactiveDataApplication.java
+++ src/main/java/com/example/reactive_data/ReactiveDataApplication.java
@@ -1,7 +1,13 @@
 package com.example.reactive_data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class ReactiveDataApplication {
@@ -9,5 +15,23 @@ public class ReactiveDataApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveDataApplication.class, args);
 	}
+
+}
+
+
+interface ReservationRepository extends ReactiveMongoRepository<Reservation, String> {
+
+	Flux<Reservation> findByReservationName(String rn);
+}
+
+@Data
+@AllArgsConstructor
+@Document
+class Reservation{
+
+	@Id
+	private String id;
+
+	private String reservationName;
 
 }
